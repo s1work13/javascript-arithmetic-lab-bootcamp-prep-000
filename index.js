fef0c1185b42b9fd8fe6932757dd1e242c15ad66
Ryan Uno <ryan@solutions-one.net>
2018-04-13 20:32:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,12 +26,5 @@ function dec(a) {
 
 function makeInt(a) {
   var n = a.toString();
-  var m = '0x2328';
-  
-  parseInt(n, 10);
-  console.log(n);
-  
-  parseInt(m, 10);
-  return m;
   return n;
 }
