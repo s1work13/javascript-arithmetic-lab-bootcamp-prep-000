67d63bfe5e9af55aadd895081eb272c593aa348f
Ryan Uno <ryan@solutions-one.net>
2018-04-13 20:30:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+  return a+b
+}
+
+function subtract(a, b) {
+  return a-b
+}
+
+function multiply(a, b) {
+  return a*b
+}
+
+function divide(a, b) {
+  return a/b
+}
+
+function inc(a) {
+  a++
+  return a
+}
+
+function dec(a) {
+  a--
+  return a
+}
+
+function makeInt(a) {
+  var n = a.toString();
+  var m = '0x2328';
+  
+  parseInt(n, 10);
+  console.log(n);
+  
+  parseInt(m, 10);
+  return m;
+  return n;
+}
